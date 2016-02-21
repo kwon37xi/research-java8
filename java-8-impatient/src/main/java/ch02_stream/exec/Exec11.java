4d494993798b2846f5b43b737c22f3c71bd44800
KwonNam Son <kwon37xi@gmail.com>
2016-02-21 14:55:07 +0900

    ch02 Exec11 - indexer를 List단위로 분할하는 방법.

--- java-8-impatient/src/main/java/ch02_stream/exec/Exec11.java
+++ java-8-impatient/src/main/java/ch02_stream/exec/Exec11.java
@@ -12,7 +12,7 @@ import java.util.stream.Stream;
  * 이는 떨어진 위치에서 평행 set 연산은 쓰레드에 안전하기 때문이다.
  * 이 작업을 어떻게 할 수 있나?
  *
- * -> 문제의 의도를 마음대로 해석해보면, Stream<ArrayList<T>> 가 가진 모든 ArrayList.size()의 총 합과 동일한 size의 ArrayList<T> all가 존재할 때
+ * -> 문제의 의도를 마음대로 해석해보면, Stream<ArrayList<T>> 가 가진 모든 ArrayList 들의 size()의 총 합과 동일한 size의 ArrayList<T> all가 존재할 때
  * Streawm<ArrayList<T>>를 parallel 로 해당 값들을 모두 all에 넣어줄 수 있어야 한다.
  *
  * -> 결론은, 이 작업을 할 수는 있지만 무의미하다. 이 일을 하려면 stream의 모든 값을 인덱스에 맞춰 일렬로 펼친뒤(flatMap) List 혹은 Array로 만들어야한다.
@@ -24,14 +24,23 @@ import java.util.stream.Stream;
 public class Exec11 {
     public static void main(String[] args) {
 
-        final Stream<ArrayList<Integer>> stream = Stream.of(new ArrayList<>(Arrays.asList(1, 2, 3)), new ArrayList<>(Arrays.asList(4, 5)), new ArrayList<>(Arrays.asList(6, 7, 8, 9, 10)));
+        final Stream<ArrayList<Integer>> stream = Stream.of(new ArrayList<>(Arrays.asList(1, 2, 3)), new ArrayList<>(Arrays.asList(4, 5, 6)), new ArrayList<>(Arrays.asList(7, 8, 9, 10)));
 
         ArrayList<Integer> all = new ArrayList<>(Arrays.asList(new Integer[10]));
 
         AtomicInteger indexer = new AtomicInteger();
 
-        stream.parallel().forEach(integers -> integers.parallelStream().forEach(i -> all.set(indexer.getAndIncrement(), i)));
+        // indexer를 모든 값 단위로 increment
+//        stream.parallel().forEach(integers -> integers.parallelStream().forEach(i -> all.set(indexer.getAndIncrement(), i)));
 
+        // or indexer를 각 List 단위로 increment
+        stream.parallel()
+            .forEach(integers -> {
+                final int parentIndex = indexer.getAndAdd(integers.size());
+                final AtomicInteger localIndexer = new AtomicInteger();
+                integers.parallelStream()
+                    .forEach(i -> all.set(parentIndex + localIndexer.getAndIncrement(), i));
+            });
         System.out.println("전체 병합 결과 : " + all);
     }
 }
