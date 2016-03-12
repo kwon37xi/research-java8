4eed5ef00dd7f6c971e5c79618d695f485f012d6
KwonNam Son <kwon37xi@gmail.com>
2016-03-12 16:07:30 +0900

    ch06 Exec05 - in progress

---
+++ java-8-impatient/src/main/java/ch06_concurrency/exec/Exec05.java
@@ -0,0 +1,11 @@
+package ch06_concurrency.exec;
+
+/**
+ * 다수의 쓰레드가 파일 집합에서 모든 단어를 읽는 애플리케이션을 작성하라, 각 단어가 어느 파일들에서 나타나는지 추적하는데
+ * ConcurrentHashMap을 사용하고, 맵을 업데이트하는데 merge 메서드를 사용하라.
+ */
+public class Exec05 {
+    public static void main(String[] args) {
+
+    }
+}
