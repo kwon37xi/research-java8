73109818a83a78790f021feda68e660a99499766
KwonNam Son <kwon37xi@gmail.com>
2016-03-13 22:54:51 +0900

    ch06 Exec05

--- java-8-impatient/src/main/java/ch06_concurrency/exec/Exec05.java
+++ java-8-impatient/src/main/java/ch06_concurrency/exec/Exec05.java
@@ -5,6 +5,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
@@ -18,41 +19,35 @@ public class Exec05 {
     public static void main(String[] args) throws InterruptedException {
         ConcurrentHashMap<String, Set<Path>> wordsInFile = new ConcurrentHashMap<>();
 
-        processWordsInFile(wordsInFile, Paths.get("java-8-impatient/alice.txt"));
-        processWordsInFile(wordsInFile, Paths.get("java-8-impatient/anne.txt"));
-        processWordsInFile(wordsInFile, Paths.get("java-8-impatient/frankenstein.txt"));
-        processWordsInFile(wordsInFile, Paths.get("java-8-impatient/kingjames_bible.txt"));
-        processWordsInFile(wordsInFile, Paths.get("java-8-impatient/war_and_peace.txt"));
+        Path[] paths = new Path[]{
+            Paths.get("java-8-impatient/alice.txt"),
+            Paths.get("java-8-impatient/anne.txt"),
+            Paths.get("java-8-impatient/frankenstein.txt"),
+            Paths.get("java-8-impatient/kingjames_bible.txt"),
+            Paths.get("java-8-impatient/war_and_peace.txt")
+        };
+        Stream.of(paths).parallel().forEach(path -> {
+            System.out.println(Thread.currentThread().getName() + "  - path : " + path);
+            final Stream<String> words = streamOfFileWords(path);
+            words.parallel().forEach(word -> {
+                wordsInFile.merge(word.toLowerCase(), new HashSet<>(), (existSet, newSetToBeAdded) -> {
+                    existSet.add(path);
+                    return existSet;
+                });
+            });
+        });
 
         System.out.println(wordsInFile);
     }
 
-    private static void processWordsInFile(ConcurrentHashMap<String, Set<Path>> wordsInFile, Path path) throws InterruptedException {
-        final Thread thread = new Thread(() -> {
-            System.out.println("path : " + path);
-            final Stream<String> words = streamOfFileWords(path);
-            words.parallel().forEach(word -> {
-                Set<Path> pathSet = new HashSet<>();
-                pathSet.add(path);
-                wordsInFile.merge(word.toLowerCase(), pathSet, (set1, set2) -> {
-                    set1.addAll(set2);
-                    return set1;
-                });
-            });
-        });
-        thread.start();
-        thread.join();
-    }
-
     public static Stream<String> streamOfFileWords(Path path) {
-        String contents = null;
+
         try {
-            contents = new String(Files.readAllBytes(path), StandardCharsets.UTF_8);
+            String contents = new String(Files.readAllBytes(path), StandardCharsets.UTF_8);
+            String[] words = contents.split("[\\P{L}]+");
+            return Stream.of(words).filter((word) -> word.length() != 0);
         } catch (IOException e) {
             throw new RuntimeException(e);
-
         }
-        String[] words = contents.split("[\\P{L}]+");
-        return Stream.of(words);
     }
 }
