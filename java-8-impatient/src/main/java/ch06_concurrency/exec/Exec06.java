8d13a8c8377dbbdb60f51ca5764cb9c06cc16c8f
KwonNam Son <kwon37xi@gmail.com>
2016-03-13 23:07:36 +0900

    ch06 Exec06 - and ConcurrentHashMap.<Path>newKeySet() 사용.

--- java-8-impatient/src/main/java/ch06_concurrency/exec/Exec06.java
+++ java-8-impatient/src/main/java/ch06_concurrency/exec/Exec06.java
@@ -11,10 +11,10 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Stream;
 
 /**
- * 다수의 쓰레드가 파일 집합에서 모든 단어를 읽는 애플리케이션을 작성하라, 각 단어가 어느 파일들에서 나타나는지 추적하는데
- * ConcurrentHashMap을 사용하고, 맵을 업데이트하는데 merge 메서드를 사용하라.
+ * copmuteIfAbsent를 사용하여 05번 문제 풀기.
+ * 이 방식을 사용하니까 불필요하게 빈 HashSet 객체를 만들 필요가 없어졌다.
  */
-public class Exec05 {
+public class Exec06 {
     public static void main(String[] args) throws InterruptedException {
         ConcurrentHashMap<String, Set<Path>> wordsInFile = new ConcurrentHashMap<>();
 
@@ -28,16 +28,11 @@ public class Exec05 {
         Stream.of(paths).parallel().forEach(path -> {
             System.out.println(Thread.currentThread().getName() + "  - path : " + path);
             final Stream<String> words = streamOfFileWords(path);
+
             // 여기서 Set을 CocurrentHashMap으로부터 만들어야만 할 것으로 보인다.
             // HashSet도 작동상의 문제는 없었으나, 이는 하나의 Key에 대한 동시 접근이 그리 많지 않았기 때문으로 보이고,
             // 값이 되는 Set을 concurrent를 처리할 수 있게 만드는 것이 원칙에 맞는 것으로 보임.
-
-            words.parallel().forEach(word -> {
-                wordsInFile.merge(word.toLowerCase(), ConcurrentHashMap.<Path>newKeySet(), (existSet, newSetToBeAdded) -> {
-                    existSet.add(path);
-                    return existSet;
-                });
-            });
+            words.parallel().forEach(word -> wordsInFile.computeIfAbsent(word.toLowerCase(), (wordToPut) -> ConcurrentHashMap.<Path>newKeySet()).add(path));
         });
 
         System.out.println(wordsInFile);
