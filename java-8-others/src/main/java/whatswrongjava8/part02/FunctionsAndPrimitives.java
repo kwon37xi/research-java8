55c52a423703dfd26fa801e3c5b89c28bbe6767d
KwonNam Son <kwon37xi@gmail.com>
2016-04-15 10:03:11 +0900

    java-8-others whatswrongjava8 part02~03 preparation

---
+++ java-8-others/src/main/java/whatswrongjava8/part02/FunctionsAndPrimitives.java
@@ -0,0 +1,7 @@
+package whatswrongjava8.part02;
+
+/**
+ * https://dzone.com/articles/whats-wrong-java-8-part-ii
+ */
+public class FunctionsAndPrimitives {
+}
---
+++ java-8-others/src/main/java/whatswrongjava8/part03/Streams.java
@@ -0,0 +1,7 @@
+package whatswrongjava8.part03;
+
+/**
+ * https://dzone.com/articles/whats-wrong-java-8-part-iii
+ */
+public class Streams {
+}
