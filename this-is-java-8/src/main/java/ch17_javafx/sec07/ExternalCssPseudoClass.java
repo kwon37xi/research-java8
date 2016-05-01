1c0618c5dc278609af424fbd9f5fc141a8d9ebd4
KwonNam Son <kwon37xi@gmail.com>
2016-05-01 22:17:50 +0900

    this-is-java-8  external_css pseudo class

---
+++ this-is-java-8/src/main/java/ch17_javafx/sec07/ExternalCssPseudoClass.java
@@ -0,0 +1,24 @@
+package ch17_javafx.sec07;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class ExternalCssPseudoClass extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/external_css_pseudo_class.fxml"));
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("/external_css_pseudo_class.css").toString());
+
+        primaryStage.setTitle("External CSS");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
