8f318b36cdedf7c5393544d611e3a7717f5f8eab
KwonNam Son <kwon37xi@gmail.com>
2016-05-05 23:38:12 +0900

    this-is-java-8  add animation

--- this-is-java-8/src/main/java/ch17_javafx/sec12/AnimationController.java
+++ this-is-java-8/src/main/java/ch17_javafx/sec12/AnimationController.java
@@ -1,5 +1,8 @@
 package ch17_javafx.sec12;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -10,17 +13,18 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class StackPaneController extends Application implements Initializable {
+public class AnimationController extends Application implements Initializable {
     @FXML private Button btnLogin;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/stackpane.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/animation.fxml"));
         primaryStage.setScene(new Scene(root));
 
         primaryStage.setWidth(350);
@@ -36,9 +40,17 @@ public class StackPaneController extends Application implements Initializable {
 
     public void handleBtnLogin(ActionEvent event) {
         try {
-            Parent login = FXMLLoader.load(getClass().getResource("/stackpane_login.fxml"));
+            Parent login = FXMLLoader.load(getClass().getResource("/animation_login.fxml"));
             StackPane root = (StackPane) btnLogin.getScene().getRoot();
+            login.setTranslateX(350); // 시작값
             root.getChildren().add(login);
+
+
+            Timeline timeline = new Timeline();
+            KeyValue keyValue = new KeyValue(login.translateXProperty(), 0);
+            KeyFrame keyFrame = new KeyFrame(Duration.millis(500), keyValue);
+            timeline.getKeyFrames().add(keyFrame);
+            timeline.play();
         } catch (IOException e) {
             e.printStackTrace();
         }
--- this-is-java-8/src/main/java/ch17_javafx/sec12/AnimationLoginController.java
+++ this-is-java-8/src/main/java/ch17_javafx/sec12/AnimationLoginController.java
@@ -1,16 +1,20 @@
 package ch17_javafx.sec12;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
+import javafx.util.Duration;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class LoginController implements Initializable {
+public class AnimationLoginController implements Initializable {
     @FXML private BorderPane login;
     @FXML private Button btnMain;
 
@@ -22,7 +26,20 @@ public class LoginController implements Initializable {
     public void handleBtnMain(ActionEvent event) {
         try {
             StackPane root = (StackPane) btnMain.getScene().getRoot();
-            root.getChildren().remove(login);
+
+            login.setTranslateX(0);
+
+            Timeline timeline = new Timeline();
+            KeyValue keyValue = new KeyValue(login.translateXProperty(), 350);
+            KeyFrame keyFrame = new KeyFrame(
+                Duration.millis(500),
+                timelineEvent -> root.getChildren().remove(login),
+                keyValue
+            );
+
+            timeline.getKeyFrames().addAll(keyFrame);
+            timeline.play();
+
         } catch (Exception e) {
             e.printStackTrace();
         }
