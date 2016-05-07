8b8dd062f306e00baca1b01b2668ce71419d285e
KwonNam Son <kwon37xi@gmail.com>
2016-05-07 15:33:19 +0900

    this-is-java-8  ch17 exec02

--- this-is-java-8/src/main/java/ch17_javafx/exec/RootController.java
+++ this-is-java-8/src/main/java/ch17_javafx/exec/RootController.java
@@ -1,9 +1,18 @@
 package ch17_javafx.exec;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.stage.Window;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -15,6 +24,27 @@ public class RootController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        addButton.setOnAction(e -> System.out.println("add button clicked"));
+        addButton.setOnAction(this::handleAddButton);
+    }
+
+    public void handleAddButton(ActionEvent event) {
+        System.out.println("add button clicked.");
+
+        try {
+            final Window primaryWindow = addButton.getScene().getWindow();
+            Stage addDialog = new Stage(StageStyle.UTILITY);
+            addDialog.initModality(Modality.WINDOW_MODAL);
+            addDialog.initOwner(primaryWindow);
+            addDialog.setTitle("추가");
+
+            final Parent addDialogForm = FXMLLoader.load(getClass().getResource("/exec_form.fxml"));
+            Scene addScene = new Scene(addDialogForm);
+            addDialog.setResizable(false);
+            addDialog.setScene(addScene);
+            addDialog.show();
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
+
     }
 }
