45001b78304944fa670d8adc280874647614d95b
KwonNam Son <kwon37xi@gmail.com>
2016-06-03 23:58:59 +0900

    this-is-java-8  ch12 thread name

---
+++ this-is-java-8/src/main/java/ch12/sec01/ThreadA.java
@@ -0,0 +1,14 @@
+package ch12.sec01;
+
+public class ThreadA extends Thread {
+    public ThreadA() {
+        setName("ThreadA");
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 2; i++) {
+            System.out.println(getName() + "가 출력한 내용");
+        }
+    }
+}
---
+++ this-is-java-8/src/main/java/ch12/sec01/ThreadB.java
@@ -0,0 +1,10 @@
+package ch12.sec01;
+
+public class ThreadB extends Thread {
+    @Override
+    public void run() {
+        for (int i = 0; i < 2; i++) {
+            System.out.println(getName() + "가 출력한 내용");
+        }
+    }
+}
