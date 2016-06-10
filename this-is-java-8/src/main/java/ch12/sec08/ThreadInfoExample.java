9de79b11652a260b0dc6d7969df3ed00ea62e6f8
KwonNam Son <kwon37xi@gmail.com>
2016-06-10 23:31:55 +0900

    this-is-java-8  ch12 thread info

---
+++ this-is-java-8/src/main/java/ch12/sec08/ThreadInfoExample.java
@@ -0,0 +1,24 @@
+package ch12.sec08;
+
+import ch12.sec07.AutoSaveThread;
+
+import java.util.Map;
+import java.util.Set;
+
+public class ThreadInfoExample {
+    public static void main(String[] args) {
+        AutoSaveThread autoSaveThread = new AutoSaveThread();
+        autoSaveThread.setName("AutoSaveThread");
+        autoSaveThread.setDaemon(true);
+        autoSaveThread.start();
+
+        Map<Thread, StackTraceElement[]> map = Thread.getAllStackTraces();
+        Set<Thread> threads = map.keySet();
+
+        for (Thread thread: threads) {
+            System.out.println("Name: " + thread.getName() + ((thread.isDaemon())? "(데몬)" : "(주)"));
+            System.out.println("\t소속그룹: " + thread.getThreadGroup().getName());
+            System.out.println();
+        }
+    }
+}
