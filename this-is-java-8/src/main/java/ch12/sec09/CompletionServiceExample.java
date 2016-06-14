f8c33d854982f9fc02d06402a215d7602a9fde97
KwonNam Son <kwon37xi@gmail.com>
2016-06-14 21:37:36 +0900

    this-is-java-8  ch12 CompletionServiceExample

---
+++ this-is-java-8/src/main/java/ch12/sec09/CompletionServiceExample.java
@@ -0,0 +1,50 @@
+package ch12.sec09;
+
+import java.util.concurrent.*;
+
+/**
+ * 결과가 순서 없이 나옴.
+ */
+public class CompletionServiceExample {
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+
+        CompletionService<Integer> completionService = new ExecutorCompletionService<>(executorService);
+
+        System.out.println("[작업 처리 요청]");
+        for (int i = 0; i < 3; i++) {
+            final int target = (i + 1) * 10;
+            completionService.submit(() -> {
+                int sum = 0;
+                for (int idx = 1; idx <= target; idx++) {
+                    sum += idx;
+                }
+
+                return sum;
+            });
+        }
+
+        System.out.println("[처리 완료된 작업 확인]");
+        executorService.submit(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    try {
+                        Future<Integer> future = completionService.take();
+                        int value = future.get();
+                        System.out.println("[처리 결과] " + value);
+                    } catch (Exception ex) {
+                        break;
+                    }
+                }
+            }
+        });
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+
+        }
+        executorService.shutdownNow();
+    }
+}
