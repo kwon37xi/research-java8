bae5a3e8f927a2fc29c8d11dd9b6771b12e385e7
KwonNam Son <kwon37xi@gmail.com>
2017-01-03 23:48:28 +0900

    lombok-practice @NonNull with IllegalArgumentException

--- lombok-practice/src/main/java/lombokpractice/nonnull/NonNullExample.java
+++ lombok-practice/src/main/java/lombokpractice/nonnull/NonNullExample.java
@@ -7,6 +7,10 @@ https://projectlombok.org/features/NonNull.html
 
 @lombok.NonNull 을 지정하면 해당 변수에 null 체크를 하고 null 이면 NullPointerException 예외 던짐.
 생성자에서는 this(), super() 호출 직후에 코드 삽입됨.
+
+* Configuration Keys
+lombok.nonNull.exceptionType = [NullPointerException | IllegalArgumentException] (default: NullPointerException).
+lombok.nonNull.flagUsage = [warning | error] (default: not set)
  */
 public class NonNullExample {
     private String name;
@@ -30,7 +34,7 @@ public class NonNullExample {
     public static void main(String[] args) {
         try {
             new NonNullExample(null);
-        } catch (NullPointerException e) {
+        } catch (IllegalArgumentException e) {
             e.printStackTrace(System.out);
         }
 
