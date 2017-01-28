7056a6b9b90b93c9b3891ce4d299f00901f00353
KwonNam Son <kwon37xi@gmail.com>
2017-01-28 11:49:56 +0900

    lombok-practice @EqualsAndHashCode

--- lombok-practice/src/main/java/lombokpractice/equals_hashcode/EqualsAndHashCodeExample.java
+++ lombok-practice/src/main/java/lombokpractice/equals_hashcode/EqualsAndHashCodeExample.java
@@ -1,5 +1,9 @@
 package lombokpractice.equals_hashcode;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
 /*
  * https://projectlombok.org/features/EqualsAndHashCode.html
  *
@@ -11,6 +15,53 @@ package lombokpractice.equals_hashcode;
  * 아무것도 상속하지 않은 상태에서 callSuper=true는 컴파일 오류를 발생시킴.
  *
  * lombok 0.10 부터 JPA 프록시에서 잘 동작할 수 있도록 canEqual를 생성함. http://www.artima.com/lejava/articles/equality.html 참조.
+ * Scala case 클래스와 롬복 @EqualsAndHashCode를 사용한 클래스를 믹스해서 상속했다면 작동에 문제가 없을 것이다.
+ * 만약 직접 equals/hashCode를 수정하려고 한다면 canEqual을 오버라이드 해야한다.
+ * onParam=@__({@AnnotationsHere}) 사용가능.
  */
+@EqualsAndHashCode(exclude = {"id", "shape"})
 public class EqualsAndHashCodeExample {
+    private transient int transientVar = 10;
+    private String name;
+    private double score;
+    private Shape shape = new Square(5, 10);
+    private String[] tags;
+    private int id;
+
+    public String getName() {
+        return this.name;
+    }
+
+    /*
+    canEqual 이 아래처럼 생성됨.
+    protected boolean canEqual(Object other) {
+        return other instanceof EqualsAndHashCodeExample;
+    }
+
+    equals()에서 아래처럼 호출됨.
+
+    public boolean equals(Object o) {
+        ...
+        if (!other.canEqual((Object)this)) return false;
+        ...
+    }
+     */
+
+    @EqualsAndHashCode
+    public static class Shape {
+        @Getter
+        @Setter
+        public String name;
+    }
+
+    @EqualsAndHashCode(callSuper = true)
+    public static class Square extends Shape {
+        private final int width, height;
+
+        public Square(int width, int height) {
+            this.width = width;
+            this.height = height;
+        }
+    }
+
 }
