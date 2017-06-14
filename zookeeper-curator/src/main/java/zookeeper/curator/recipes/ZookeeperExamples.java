e7276cc5389186dc2f9383dedb08811d31cc8fa4
KwonNam Son <kwon37xi@gmail.com>
2017-06-14 18:15:21 +0900

    zookeeper-curator Shared Reentrant Lock - runExample 추가

---
+++ zookeeper-curator/src/main/java/zookeeper/curator/recipes/ZookeeperExamples.java
@@ -0,0 +1,5 @@
+package zookeeper.curator.recipes;
+
+public class ZookeeperExamples {
+    public static final String ZOOKEEPER_CONNECTION = "127.0.0.1:2181";
+}
--- zookeeper-curator/src/main/java/zookeeper/curator/recipes/locks/sharedreentrant/SharedReentrantLockExample.java
+++ zookeeper-curator/src/main/java/zookeeper/curator/recipes/locks/sharedreentrant/SharedReentrantLockExample.java
@@ -6,32 +6,35 @@ import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.framework.recipes.locks.InterProcessMutex;
 import org.apache.curator.retry.ExponentialBackoffRetry;
 import org.slf4j.Logger;
+import zookeeper.curator.recipes.ZookeeperExamples;
 
-import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
+/**
+ * <code>gradlew runExample -PrunMain=zookeeper.curator.recipes.locks.sharedreentrant.SharedReentrantLockExample -DappName=[appName]</code>
+ */
 public class SharedReentrantLockExample {
     private static final Logger log = getLogger(SharedReentrantLockExample.class);
 
     public static void main(String[] args) {
-        int appNum = new Random().nextInt();
-        log.info("Application app Num - {}", appNum);
+        String appName = System.getProperty("appName", "unknown");
+        log.info("Application app Num - {}", appName);
         RetryPolicy retryPolicy = new ExponentialBackoffRetry(1000, 3);
         InterProcessMutex mutex = null;
-        try (CuratorFramework client = CuratorFrameworkFactory.newClient("127.0.0.1:2181", retryPolicy)) {
+        try (CuratorFramework client = CuratorFrameworkFactory.newClient(ZookeeperExamples.ZOOKEEPER_CONNECTION, retryPolicy)) {
             client.start();
             mutex = new InterProcessMutex(client, "/examples/locks");
-            log.info("Waiting for lock - {}", appNum);
+            log.info("Waiting for lock - {}", appName);
 
             if (!mutex.acquire(10, TimeUnit.SECONDS)) {
-                log.info("Lock Fail - {}", appNum);
+                log.info("Lock Fail - {}", appName);
             } else {
                 try {
-                    log.info("Lock work - {} wait for 8 seconds", appNum);
+                    log.info("Lock work - {} wait for 8 seconds", appName);
                     TimeUnit.SECONDS.sleep(8);
-                    log.info("Lock end - {}", appNum);
+                    log.info("Lock end - {}", appName);
                 } finally {
                     mutex.release();
                 }
