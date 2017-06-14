49217e6898f8c1feafad14cecb48480a09738d69
KwonNam Son <kwon37xi@gmail.com>
2017-06-14 18:31:16 +0900

    zookeeper-curator Shared Lock

--- zookeeper-curator/src/main/java/zookeeper/curator/recipes/locks/sharedreentrant/SharedLockExample.java
+++ zookeeper-curator/src/main/java/zookeeper/curator/recipes/locks/sharedreentrant/SharedLockExample.java
@@ -3,7 +3,7 @@ package zookeeper.curator.recipes.locks.sharedreentrant;
 import org.apache.curator.RetryPolicy;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
-import org.apache.curator.framework.recipes.locks.InterProcessMutex;
+import org.apache.curator.framework.recipes.locks.InterProcessSemaphoreMutex;
 import org.apache.curator.retry.ExponentialBackoffRetry;
 import org.slf4j.Logger;
 import zookeeper.curator.recipes.ZookeeperExamples;
@@ -13,23 +13,23 @@ import java.util.concurrent.TimeUnit;
 import static org.slf4j.LoggerFactory.getLogger;
 
 /**
- * <a href="http://curator.apache.org/curator-recipes/shared-reentrant-lock.html">Shared Reentrant Lock - 재진입이 가능한 공유 Lock</a>
+ * <a href="http://curator.apache.org/curator-recipes/shared-lock.html">Shared Lock - 공유 Lock(재진입 불가)</a>
  *
- * <code>gradlew runExample -PrunMain=zookeeper.curator.recipes.locks.sharedreentrant.SharedReentrantLockExample -DappName=[appName]</code>
+ * <code>gradlew runExample -PrunMain=zookeeper.curator.recipes.locks.sharedreentrant.SharedLockExample -DappName=[appName]</code>
  */
-public class SharedReentrantLockExample {
-    private static final Logger log = getLogger(SharedReentrantLockExample.class);
+public class SharedLockExample {
+    private static final Logger log = getLogger(SharedLockExample.class);
 
     public static void main(String[] args) {
         String appName = System.getProperty("appName", "unknown");
         log.info("Application app Num - {}", appName);
         RetryPolicy retryPolicy = new ExponentialBackoffRetry(1000, 3);
 
-        InterProcessMutex mutex = null;
+        InterProcessSemaphoreMutex mutex = null;
 
         try (CuratorFramework client = CuratorFrameworkFactory.newClient(ZookeeperExamples.ZOOKEEPER_CONNECTION, retryPolicy)) {
             client.start();
-            mutex = new InterProcessMutex(client, "/examples/locks");
+            mutex = new InterProcessSemaphoreMutex(client, "/examples/locks");
             log.info("Waiting for lock - {}", appName);
 
             if (!mutex.acquire(20, TimeUnit.SECONDS)) {
@@ -39,13 +39,14 @@ public class SharedReentrantLockExample {
                     log.info("Lock work - {} wait for 8 seconds", appName);
                     TimeUnit.SECONDS.sleep(8);
 
-                    // reentrant lock 테스트
+                    /*
+                    - reentrant가 안되기 때문에 아래 코드를 실행하면 계속 lock 상태 유지됨.
                     log.info("Lock inner again - {} wait for 5 seconds", appName);
                     mutex.acquire();
                     TimeUnit.SECONDS.sleep(5);
                     mutex.release();
                     log.info("Lock inner released - {}", appName);
-
+                    */
                     log.info("Lock outer end - {}", appName);
                 } finally {
                     mutex.release();
